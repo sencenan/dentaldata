e3a17df882813301853647aae0b47845d649d5f7
sen.cenan@gmail.com <sen.cenan@gmail.com>
2016-04-14 17:35:12 -0400

    m2

--- ddata.hs
+++ ddata.hs
@@ -1,41 +1,27 @@
---import Graphics.Rendering.Chart
---import Graphics.Rendering.Chart.Backend.Cairo
---import Data.Default.Class
---import Control.Lens
---import System.Environment(getArgs)
+import Control.Lens
+import Data.Maybe
+import Data.List
+import Data.List.Split
+import Data.String.Utils
+import Graphics.Rendering.Chart
+import Graphics.Rendering.Chart.Layout
+import Graphics.Rendering.Chart.Backend.Cairo
+--import Graphics.Rendering.Chart.Backend.Diagrams
+import Graphics.Rendering.Chart.Easy
+import Text.Read
+
+import qualified Data.Map as Map
+import qualified Data.Matrix as M
+import qualified Data.Text as T
+import qualified Data.Vector.Storable as V
+import qualified Numeric.Statistics as S
+
+import System.Environment(getArgs)
 import Text.CSV
 import Text.Parsec.Error
-import qualified Data.Text as T
-import qualified Data.Matrix as M
-import qualified Data.Map as Map
-import Data.Maybe
 
---chart = toRenderable layout
---   where
---      values = [ ("Mexico City", 19.2, e),
---         ("Mumbai", 12.9, e),
---         ("Sydney", 4.3, e),
---         ("London", 8.3, e),
---         ("New York", 8.2, e1),
---         ("Beijing", 15.2, e) ]
---      e = 0
---      e1 = 25
---      pitem (s,v,o) = pitem_value .~ v
---         $ pitem_label .~ s
---         $ pitem_offset .~ o
---         $ def
---      layout = pie_title .~ "Relative Population"
---         $ pie_plot . pie_data .~ map pitem values
---         $ def
-
---main1 ["svg"] = renderableToFile def "example5.svg" chart
---main1 ["png"] = renderableToFile def "example5.png" chart
-
---main = getArgs >>= main1
-
-main :: IO ()
 main = do
-   csvData <- parseCSVFromFile "data/data.csv"
+   csvData <- parseCSVFromFile "data/test-data.csv"
    case csvData of
       Left err
          -> sequence_
@@ -43,29 +29,71 @@ main = do
             . errorMessages
             $ err
       Right (header:contents)
-          -> mapM_ processPair comb
-         -- -> putStrLn . show $ comb
-            where
-               comb = [ x
-                  | x <- mapM (const (zip header cols)) [1..2]
-                  , head x < head (tail x) ]
-               cols = M.toLists . M.transpose . M.fromLists
-                  . (take (length contents - 1)) . trimCSV $ contents
+         -> process header contents
+         >> (putStrLn ".")
 
-processPair ((hx, x):(hy, y):_) = do
-   (putStrLn $ hx ++ " vs. " ++ hy)
-       >> (putStrLn . show . df $ x)
-       >> (putStrLn . show . df $ y)
+opt = def{_fo_size=(800, 9000)}
+
+process header contents =
+   (mapM_ renderCharts (chunksOf 10 comb))
+   >> (mapM_ processCorcoeff comb)
+   where
+      comb = sortBy sortPairs [ x
+         | x <- mapM (const (zip header cols)) [1..2]
+         , head x < head (tail x) ]
+      cols = M.toLists . M.transpose . M.fromLists
+         . (take (length contents - 1)) . trimCSV $ contents
+
+renderCharts comb = renderableToFile opt ("output/" ++ title ++ ".png")
+   $ renderStackedLayouts
+   $ slayouts_layouts .~ map (StackedLayout . processPair) comb
+   $ slayouts_compress_legend .~ False
+   $ def
+   where
+      title = fixFileName $ t1 ++ " --> " ++ t2
+         where
+            t1 = fst . head . head $ comb
+            t2 = fst . head . reverse . head $ comb
+
+sortPairs :: [(String, [String])] -> [(String, [String])] -> Ordering
+sortPairs a b = compare left right
+   where
+      left = fst . head $ a
+      right = fst . head $ b
+
+processCorcoeff ((hx, x):(hy, y):_) = putStrLn $
+   hx ++ "," ++ hy ++ "," ++ (show cvSpear) ++ "," ++ (show cv)
       where
-         df vs = catMaybes $ digitize mv vs
-            where mv = foldl mkValueMap Map.empty vs
+         cvSpear = S.spearman (V.fromList dx) (V.fromList dy)
+         cv = S.corcoeff (V.fromList dx) (V.fromList dy)
+         dx = digitize x
+         dy = digitize y
+
+processPair ((hx, x):(hy, y):_) = chartPair title pts
+   where
+      title = hx ++ "-x-" ++ hy
+      pts = zip (digitize x) (digitize y)
+
+chartPair title pts = layout_title .~ title
+   $ layout_plots .~ [toPlot (
+      plot_points_values .~ pts
+      $ plot_points_style .~ filledCircles 2 (opaque red)
+      $ def
+   )]
+   $ def
 
 trimCSV = map . map $ trimCol
 trimCol = T.unpack . T.strip . T.pack
+fixFileName x = replace "." "_" $ replace "/" "_" x
 
 -- data stream to digital data stream
-digitize mv vs = map g vs where g x = Map.lookup x mv
+digitize vs = catMaybes $ dz mv vs
+   where
+      mv = foldl mkValueMap Map.empty vs
+      dz mv vs = map g vs where g x = Map.lookup x mv
 
 mkValueMap valMap v = case Map.lookup v valMap of
    Just value -> valMap
-   Nothing -> Map.insert v (Map.size valMap) valMap
+   Nothing -> case readMaybe v :: Maybe Double of
+      Just fv -> Map.insert v fv valMap
+      Nothing -> Map.insert v (fromIntegral (Map.size valMap)) valMap
